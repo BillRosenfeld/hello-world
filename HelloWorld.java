e02c7824a85e602841ffbab9614e952e62bcd126
Bill <bill.rosenfeld@me.com>
2023-02-12 11:41:28 -0500

    learning

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        System.out.print("Hello World!");
+    private static final String FILE_NAME = "README.md";
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+        BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        reader.close();
     }
+
 }
